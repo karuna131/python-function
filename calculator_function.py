93609ed9e48e5297a83b92019ad96fe02c3b467c
karuna131 <78745446+karuna131@users.noreply.github.com>
2021-03-14 12:16:35 +0530

    Add files via upload

---
+++ calculator_function.py
@@ -0,0 +1,54 @@
+#1 calculator question
+
+# num1=int(input("Enter first number : "))
+# num2=int(input("Enter second number : "))
+# select=input("Enter operator symbol : ")
+
+# def add(num1,num2):
+#     return num1+num2
+# def sub(num1,num2):
+#     return num1-num2
+# def multiply(num1,num2):
+#     return num1*num2
+# def divide(num1,num2):
+#     return num1/num2
+
+# if select=="+":
+#     print("addition of both numbers  :", num1+num2)
+# elif select=="-":
+#     print("subtraction of both numbers  :", num1-num2)
+# elif select=="*":
+#     print("mutiplication of both numbers  :", num1*num2)
+# elif select=="/":
+#     print("dividetion of both numbers  :", num1/num2)
+# else:
+#     print("invailid")
+
+
+
+#2 calculator question
+
+value1=int(input("Enter first number : "))
+value2=int(input("Enter second number : "))
+
+
+def calculator(value1,value2):
+    if value3=="+":
+        return value1+value2
+    elif value3=="-":
+        return value1-value2
+    elif value3=="/":
+        return value1/value2
+    elif value3=="*":
+        return value1*value2
+
+add_result = value1+value2
+print("add_result : ",add_result)
+subtract_result=value1-value2
+print("subtract_result : ",subtract_result)
+multiply_result = value1*value2
+print("multiply_result : ",multiply_result)
+divide_result = value1/value2
+print("divide_result : ",divide_result)
+
+
