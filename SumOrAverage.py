650ab11f53aa8e7b36d5287dda9ac9308810eeb2
karuna131 <78745446+karuna131@users.noreply.github.com>
2021-03-14 12:01:35 +0530

    Add files via upload

---
+++ SumOrAverage.py
@@ -0,0 +1,7 @@
+first=int(input("Enter first number : "))
+second=int(input("Enter second number : "))
+third=int(input("Enter third number : "))
+print("sum of three numbers : ", first+second+third)
+a=(first+second+third)/3
+print("Average of sum : ", a)
+
