11613cb26eae49061c0f4a3efba7b1542ff8c5eb
karuna131 <78745446+karuna131@users.noreply.github.com>
2021-03-14 12:14:21 +0530

    Add files via upload

---
+++ Default_parameter_value.py
@@ -0,0 +1,18 @@
+#1
+
+def attendance(name,status="absent"):
+    print(name,"is",status," today")
+
+attendance("kartik","present")
+attendance("sonu")
+attendance("vishal","present")
+attendance("umesh") 
+
+
+#2
+
+def info(age,name="chhaya"):
+    print(age,"or",name)
+info(20, "karu")
+info(23)
+info(21, "sallu")
