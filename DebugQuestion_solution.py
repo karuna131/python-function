b596b39984f52f9980ddae12a158987bf7acfcaf
karuna131 <78745446+karuna131@users.noreply.github.com>
2021-03-14 12:15:01 +0530

    Add files via upload

---
+++ DebugQuestion_solution.py
@@ -0,0 +1,48 @@
+
+#1
+
+def calculate_sum(a,b):
+    sum = a+b
+    print(sum)
+calculate_sum(4,5)
+ 
+
+#2
+
+def multi(a,b):
+    multiply=a*b
+    return multiply
+print(multi(3,4)) 
+
+
+#3
+
+def Avg(number1,number2,number3):
+    avg=number1+number2+number3/3
+    print(avg)
+Avg(1,3,2)
+
+
+#4
+
+def voter(age):
+    if age < 18:
+        print("eligible")
+    else:
+        print("not eligible")
+voter(20)
+
+
+
+#5
+
+def distance(kms):
+    if kms < 20:
+        print("close")
+    elif kms < 50:
+        print("median")
+    else:
+        Print("far")
+distance(30) 
+ 
+ 
