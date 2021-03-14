171546322d992b68ecdae9deee3e6bfab2925bff
karuna131 <78745446+karuna131@users.noreply.github.com>
2021-03-14 12:08:26 +0530

    Add files via upload

---
+++ return_function.py
@@ -0,0 +1,13 @@
+def add_numbers(numx, numy):
+    sum=numx+numy
+    return sum
+sum1=add_numbers(12,45)
+print(sum1)
+sum2=add_numbers(20, 80)
+print(sum2)
+a=20
+b=10
+sum3=add_numbers(a,b)
+print(sum3)
+sum4=add_numbers(10,50)/add_numbers(5,6)
+print(sum4) 
