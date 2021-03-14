ad8d8807c7be15d944c6d9ec58d054e0cd4859c7
karuna131 <78745446+karuna131@users.noreply.github.com>
2021-03-14 12:07:27 +0530

    Add files via upload

---
+++ sort_list_fuction.py
@@ -0,0 +1,15 @@
+def sort_function():
+    unorder_list=[6,8,4,3,9,56,0,34,0,7,15]
+    unorder_list.sort()
+    print(unorder_list)
+sort_function()
+
+
+
+
+def sort_function():
+    unorder_list=[6,8,4,3,9,56,0,34,0,7,15]
+    unorder_list.sort(reverse=True)
+    print(unorder_list)
+sort_function()
+
